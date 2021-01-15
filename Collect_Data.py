eeffdc5f19b83d6f392ae79094ac994d366d3d2c
dankevich.te <Timofey.Dankevich@gmail.com>
2021-01-15 16:49:43 +0200

    The final version.

--- Collect_Data.py
+++ Collect_Data.py
@@ -3,7 +3,7 @@ import Parser_Web
 
 
 def collect_data(query):
-    Parser_Twitter.get_data_from_Twitter(query, 50)
+    Parser_Twitter.get_data_from_Twitter(query, 100)
     print('Data was obtained from Twitter.')
-    Parser_Web.get_data_from_Web(query, 50)
+    Parser_Web.get_data_from_Web(query, 100)
     print('Data was obtained from Google.')
